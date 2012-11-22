e54f98bc5c781114bf2443412d3e6acab5ffd886
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2012-11-22 15:16:26 -0300

    Saque columna Fecha de Operacion

---
+++ settings_original.php
@@ -0,0 +1,8 @@
+<?php
+$host = "64.22.83.58";
+$user = "yplaya_pablodiaz";
+$pass = "30YplayA";
+$db   = "yplaya_db";
+$select_db_error = "No se pudo establecer conexión con la base de datos";
+$query_error = "No se pudo consultar la base de datos. Intente luego";
+?>
